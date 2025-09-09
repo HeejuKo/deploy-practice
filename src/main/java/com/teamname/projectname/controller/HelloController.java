36ab1c1947f791916f6c4dd5b1e84fa11a7de6e6
HeejuKo <khjg00gle96@gmail.com>
2025-09-10 04:29:39 +0900

    ✨ [Feat] hello2 API 구현
    [Feat] hello2 API 구현

--- src/main/java/com/teamname/projectname/controller/HelloController.java
+++ src/main/java/com/teamname/projectname/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello, World!";
     }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "GitHub Actions를 통한 CI/CD 파이프라인 구축, 정말 쉽죠? ^_^";
+    }
 }
